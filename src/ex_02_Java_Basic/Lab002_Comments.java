c2bae73744508b1e7246cff87701f2cc65221ed9
navatheatul-ux <navatheatul@gmail.com>
2026-01-20 15:57:46 +0530

    Adding new concept - Commnets

---
+++ src/ex_02_Java_Basic/Lab002_Comments.java
@@ -0,0 +1,25 @@
+package ex_02_Java_Basic;
+
+public class Lab002_Comments {
+
+    public static void main(String[] args) {
+
+        // This is single line comment
+       // This is also a single line comment
+
+        //This is also a single line comment
+        //
+       /* This is also a multi line comment
+        This is also a multi line comment
+        This is also a multi line comment
+        This is also a multi line comment
+        This is also a multi line comment
+
+        seygffbewurhuib buehrwr weuh   uwehr ewurrh dgs
+        jsjdfsf skfsf
+        =
+        */
+
+
+    }
+}
